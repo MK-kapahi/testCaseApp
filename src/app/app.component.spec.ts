85ec557dbd31e440df76b9aa6e036b362397acb8
MK-kapahi <Muskan@chicmic.co.in>
2023-03-24 17:53:20 +0530

    Cases Written

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+//test-suit
+describe('AppComponent', () => {  
+
+  let component = new AppComponent;
+  //1st this will execute
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,6 +18,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  //test-case 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -32,4 +37,14 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('testcaseApp app is running!');
   });
-});
+  //use x for excluding a particular testcase
+  it('testcase',()=>{
+  
+    expect(true).toBe(true);
+  })
+
+  it('Show Alert Message',()=>{
+    expect(component.showMsg("Heyyy")).toBe("Heyyy");
+
+  })
+});                                                                                                                                                                                                                   
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testcaseApp';
+
+  showMsg(msg: string): string{
+    return msg;
+  }
 }
